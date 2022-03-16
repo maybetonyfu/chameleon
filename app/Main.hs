e3dadd397378770657e87d511cc83f7bea65b0d7
Tony Fu <tonyfu.dev@gmail.com>
2022-03-16 14:45:22 +1100

    Relocate erver module

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,51 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
+import Web.Scotty
+    ( json,
+      body,
+      post,
+      scotty,
+      ScottyM,
+      file,
+      get,
+      param,
+      regex,
+      setHeader )
+import qualified Data.ByteString.Lazy.Char8 as BS
+import JsonInstance
+import Run hiding (main)
+import Data.Monoid (mconcat)
+import qualified Data.Text.Lazy as T
 
-import qualified Run
+main = scotty 3000 (typecheck >> home >> js >> css >> favicon)
 
-main :: IO ()
-main = Run.main
+
+typecheck :: ScottyM ()
+typecheck = post "/typecheck" $ do
+    content <- body
+    let result = processFile (BS.unpack content)
+    json result
+
+
+home :: ScottyM ()
+home = get "/" $ do
+    file "static/build/index.html"
+
+js :: ScottyM ()
+js = get (regex  "^.*\\.js") $ do
+    path <- param "0"
+    let filename = "static/build" `T.append` path
+    setHeader "Content-Type" "application/javascript"
+    file (T.unpack filename)
+
+css :: ScottyM ()
+css = get (regex  "^.*\\.css") $ do
+    path <- param "0"
+    let filename = "static/build" `T.append` path
+    setHeader "Content-Type" "text/css"
+    file (T.unpack filename)
+
+favicon :: ScottyM ()
+favicon = get "/favicon.ico" $ do
+    setHeader "Content-Type" "image/vnd.microsoft.icon"
+    file "static/build/favicon.ico"
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Chameleon
+
+## Build project
+```
+stack build
+```
+
+## Run Server
+```
+stack run
+```
+and the server will be available at http://localhost:3000
