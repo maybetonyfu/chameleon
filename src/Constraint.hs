6ecc36364967c15c65cbbf25975300f349227900
Tony Fu <tonyfu.dev@gmail.com>
2022-05-20 17:25:02 +1000

    Add debugging import

--- src/Constraint.hs
+++ src/Constraint.hs
@@ -2,7 +2,7 @@ module Constraint where
 import qualified Data.Map as Map
 import Data.Maybe
 import Kanren
-
+import Debug.Trace
 
 deriveRequirement :: Subst -> Subst
 deriveRequirement subs =
@@ -10,19 +10,16 @@ deriveRequirement subs =
       walkUpdateTags :: Term -> Subst -> Subst
       walkUpdateTags Atom {} subs = subs
       walkUpdateTags Unit subs = subs
-      walkUpdateTags (Pair x y _) subs =
-        walkUpdateTags y (walkUpdateTags x subs)
+      walkUpdateTags (Pair x y _) subs = walkUpdateTags y (walkUpdateTags x subs)
       walkUpdateTags (Var x tags) subs =
         case Map.lookup x subs of
           Nothing -> subs
-          Just t ->
-            walkUpdateTags t (Map.insert x (appendTags tags t) subs)
+          Just t -> walkUpdateTags t (Map.insert x (appendTags tags t) subs)
       walkSyncTermTags :: Subst -> Subst
       walkSyncTermTags subs =
         let addTerm (Var v tags) mapping = Map.insertWith (++) (var v) tags mapping
             addTerm (Atom a tags) mapping = Map.insertWith (++) (atom a) tags mapping
-            addTerm (Pair x y tags) mapping =
-              Map.insertWith (++) (pair (setTags [] x) (setTags [] y)) tags (addTerm y . addTerm x $ mapping)
+            addTerm (Pair x y tags) mapping = Map.insertWith (++) (pair (setTags [] x) (setTags [] y)) tags (addTerm y . addTerm x $ mapping)
             addTerm _ mapping = mapping
             lookUpAndSetTag (Var v _) mapping = Var v (fromMaybe [] (Map.lookup (var v) mapping))
             lookUpAndSetTag (Atom a _) mapping = Atom a (fromMaybe [] (Map.lookup (atom a) mapping))
@@ -35,5 +32,5 @@ deriveRequirement subs =
             tagMap = foldr addTerm Map.empty (Map.elems subs ++ Map.elems subs)
             reinsertTags (key, term) sub =
               Map.insert key (lookUpAndSetTag term tagMap) sub
-         in foldr reinsertTags subs (Map.toList subs)
+         in trace (show tagMap) foldr reinsertTags subs (Map.toList subs)
    in walkSyncTermTags $ foldr (walkUpdateTags . var) subs allKeys
